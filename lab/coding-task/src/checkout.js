e5724ceb8748a39cdb227ad500e5e8cad76ef619
Israel <70981991+israeljuri@users.noreply.github.com>
2026-08-16 20:29:02 +0100

    chore: keep cost-accounting branch focused

--- lab/coding-task/src/checkout.js
+++
@@ -1,7 +0,0 @@
-import { subtotalCents } from './cart.js';
-import { applyDiscount } from './pricing.js';
-
-export function orderTotalCents(cart, discountPercent, taxPercent) {
-  // TODO: implement checkout total
-  throw new Error('TODO: implement orderTotalCents');
-}
